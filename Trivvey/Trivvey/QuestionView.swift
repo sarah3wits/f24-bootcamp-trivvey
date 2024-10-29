b2f12cc51d883e7eb84dfeb1d59cea2d3c13f4b9
Sarah Threewits <sarah3wits@gmail.com>
2024-10-29 16:45:10 -0400

    trivvey

---
+++ Trivvey/Trivvey/QuestionView.swift
@@ -0,0 +1,54 @@
+struct QuestionView: View {
+    let question: Question
+    
+    @State private var selectedAnswer: String?
+    
+    var body: some View {
+        VStack {
+            Text(question.question)
+                .font(.title)
+            VStack {
+                Button {
+                    selectedAnswer = "A"
+                } label: {
+                    Text(question.A)
+                }
+                
+                Button {
+                    selectedAnswer = "B"
+                } label: {
+                    Text(question.B)
+                }
+                
+                Button {
+                    selectedAnswer = "C"
+                } label: {
+                    Text(question.C)
+                }
+                
+                Button {
+                    selectedAnswer = "D"
+                } label: {
+                    Text(question.D)
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+            if let selectedAnswer = selectedAnswer {
+                if selectedAnswer == question.answer {
+                    Text("Correct!")
+                        .foregroundColor(.green)
+                        .bold()
+                } else {
+                    Text("Incorrect!")
+                        .foregroundColor(.red)
+                        .bold()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    QuestionView(question: .example)
+}
